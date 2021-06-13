7f0e8246d1b1e7ca3c58b070572991f2454b2f74
Benjamin <28043421+benjaminadk@users.noreply.github.com>
2021-06-12 23:05:04 -0700

    fix og:image on blog posts

--- frontend/components/Layout/Meta.js
+++ frontend/components/Layout/Meta.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 
 import { createStructuredData } from '@/lib/utils'
-import { CLIENT_URL, FACEBOOK_APP_ID } from '@/lib/constants'
+import { CLIENT_URL, CLOUDINARY_URL, FACEBOOK_APP_ID } from '@/lib/constants'
 
 function Meta({ pageProps }) {
   const router = useRouter()
@@ -44,7 +44,7 @@ function Meta({ pageProps }) {
   const route = useMemo(() => {
     return router?.asPath || null
   }, [])
-
+  console.log(image)
   return (
     <Head>
       <meta charSet='utf-8' />
@@ -70,7 +70,10 @@ function Meta({ pageProps }) {
       <meta property='og:description' content={description} />
       {image ? (
         <>
-          <meta property='og:image' content={image.url} />
+          <meta
+            property='og:image'
+            content={pageProps.blogPost ? CLOUDINARY_URL + image.url : image.url}
+          />
           <meta property='og:image:type' content={image.mime} />
           <meta property='og:image:width' content={image.width} />
           <meta property='og:image:height' content={image.height} />
