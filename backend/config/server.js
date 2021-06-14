0667659061f7963f67fe55aa0b79a394a0fde0d8
Benjamin <28043421+benjaminadk@users.noreply.github.com>
2021-06-14 01:28:37 -0700

    fix admin public url

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('ADMIN_HOST'),
   port: env.int('ADMIN_PORT'),
+  url: env('ADMIN_URL'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
